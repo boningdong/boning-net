f2f3534fbb4e4eb7c2aaade4d4b3aa09a516ef79
Boning Dong <boning.d.dev@gmail.com>
2025-08-16 22:54:48 -0700

    Modernized all JS and removed Jqueries.

--- assets/js/showcase/artwork.js
+++ assets/js/showcase/artwork.js
@@ -1,14 +1,19 @@
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function(){
     format_showcase();
 });
-$('.image').on('load', format_showcase);
+document.querySelectorAll('.image').forEach(function(image) {
+    image.addEventListener('load', format_showcase);
+});
 function format_showcase() {
-    $('.grid').masonry({
-        itemSelector: '.grid-item',
-        columnWidth: 250,
-        gutter: 20,
-        horizontalOrder: true,
-        fitWidth: true,
-        transitionDuration: '0.35s'
-    });
+    var gridElement = document.querySelector('.grid');
+    if (gridElement) {
+        new Masonry(gridElement, {
+            itemSelector: '.grid-item',
+            columnWidth: 250,
+            gutter: 20,
+            horizontalOrder: true,
+            fitWidth: true,
+            transitionDuration: '0.35s'
+        });
+    }
 }
